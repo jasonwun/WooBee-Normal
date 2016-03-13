1e892385e0d7353b78b86cda137b3d675776acd7
Junda Yin <yinjunda.cool@gmail.com>
2016-03-13 19:23:36 -0400

    Use normal pivot control avoid unhandled Win32 exception

--- WooBee-Normal/XAML/Message.xaml.cs
+++ WooBee-Normal/XAML/Message.xaml.cs
@@ -28,27 +28,9 @@ namespace WooBee_Normal
         public Message()
         {
             this.InitializeComponent();
-            CommentListview.ItemsSource = commentsource;
-            MentionsListview.ItemsSource = mentionssource;
+            //CommentListview.ItemsSource = commentsource;
+            //MentionsListview.ItemsSource = mentionssource;
         }
 
-        private void OnCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
-        {
-            var pointx2 = Window.Current.CoreWindow.PointerPosition.X;
-            if (pointx2 > pointx1)
-            {
-                if (myPivot.SelectedIndex == 0)
-                    myPivot.SelectedIndex = 1;
-                else
-                    myPivot.SelectedIndex = 0;
-            }
-            else
-                return;
-        }
-
-        private void OnStarting(object sender, ManipulationStartingRoutedEventArgs e)
-        {
-            pointx1 = Window.Current.CoreWindow.PointerPosition.X;
-        }
     }
 }
