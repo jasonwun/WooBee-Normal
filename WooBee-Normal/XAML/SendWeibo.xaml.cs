7fc1b312e54b53fc82924bc270b15668d4b0f0db
Junda Yin <yinjunda.cool@gmail.com>
2016-03-28 15:21:09 -0400

    Enable to send multiple images

--- WooBee-Normal/XAML/SendWeibo.xaml.cs
+++ WooBee-Normal/XAML/SendWeibo.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
@@ -57,7 +58,8 @@ namespace WooBee_Normal
         private bool _isEmojiActivated = false;
         private static ObservableCollection<BitmapImage> _emojisource = App._emoticonSource;
         private static Dictionary<string, string> _reverseEmojiDict = new Dictionary<string, string>();
-        private static StorageFile file { get; set; }
+        private static IReadOnlyList<StorageFile> files { get; set; }
+        private static string pic_ids = "";
         #endregion
 
         #region Event
@@ -137,18 +139,26 @@ namespace WooBee_Normal
             }
             else
             {
-                if(file == null)
+                if(files.Count == 0)
                 {
                     contentTextBox.LostFocus += LostFocus;
                     await HttpPost(contentTextBox.Text);
                     await Task.Delay(500);
                     Frame.GoBack();
                 }
-                else
+                else if(files.Count == 1)
+                {
+                    if (abc == "写点啥吧")
+                        abc = "分享图片";
+                    await HttpPostWithImage(abc);
+                    Frame.GoBack();
+                }
+
+                else if(files.Count > 1)
                 {
                     if (contentTextBox.Text == "写点啥吧")
                         contentTextBox.Text = "分享图片";
-                    await HttpPostWithImage(contentTextBox.Text);
+                    await HttpPostWithMultipleImage(abc);
                     Frame.GoBack();
                 }
                 
@@ -236,15 +246,22 @@ namespace WooBee_Normal
             open.FileTypeFilter.Add(".jpg");
 
             // Open a stream for the selected file
-            file = await open.PickSingleFileAsync();
-            if(file != null)
+            files = await open.PickMultipleFilesAsync();
+            if(files != null)
             {
+                if(files.Count == 1)
+                {
+                    IRandomAccessStream fileStream = await files[0].OpenAsync(FileAccessMode.Read);
+                    BitmapImage bitmapImage = new BitmapImage();
+                    await bitmapImage.SetSourceAsync(fileStream);
+                    image.Source = bitmapImage;
+                    image.Visibility = Visibility.Visible;
+                }
+                 else if(files.Count > 1)
+                {
+
+                }
                 
-                IRandomAccessStream fileStream = await file.OpenAsync(FileAccessMode.Read);
-                BitmapImage bitmapImage = new BitmapImage();
-                await bitmapImage.SetSourceAsync(fileStream);
-                image.Source = bitmapImage;
-                image.Visibility = Visibility.Visible;
             }
             
         }
@@ -281,7 +298,7 @@ namespace WooBee_Normal
         {
             string posturi = "https://upload.api.weibo.com/2/statuses/upload.json?access_token=" + App.access_token;
             HttpClient httpClient = new HttpClient();
-            FileRandomAccessStream stream = (FileRandomAccessStream)await file.OpenAsync(FileAccessMode.Read);
+            FileRandomAccessStream stream = (FileRandomAccessStream)await files[0].OpenAsync(FileAccessMode.Read);
             HttpStreamContent streamContent1 = new HttpStreamContent(stream);
             HttpStringContent stringContent = new HttpStringContent(postMsg);
             HttpMultipartFormDataContent fileContent = new HttpMultipartFormDataContent();
@@ -289,6 +306,44 @@ namespace WooBee_Normal
             fileContent.Add(streamContent1, "pic", "pic.jpg");
             HttpResponseMessage response = await httpClient.PostAsync(new Uri(posturi), fileContent);
             string responString = await response.Content.ReadAsStringAsync();
+        }
+
+        private async Task HttpPostWithMultipleImage(string abc)
+        {
+            string posturi = "https://api.weibo.com/2/statuses/upload_url_text.json?access_token=" + App.weico_access_token;
+            foreach (StorageFile file in files)
+            {
+                FileRandomAccessStream stream = (FileRandomAccessStream)await file.OpenAsync(FileAccessMode.Read);
+                HttpStreamContent streamContent1 = new HttpStreamContent(stream);
+                await UploadImages(streamContent1);
+
+            }
+            HttpClient httpClient = new HttpClient();
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, new Uri(posturi));
+            HttpFormUrlEncodedContent postData = new HttpFormUrlEncodedContent(
+                new List<KeyValuePair<string, string>>
+                {
+                        new KeyValuePair<string, string>("status", abc),
+                        new KeyValuePair<string, string>("pic_id", pic_ids),
+
+                }
+            );
+            request.Content = postData;
+            HttpResponseMessage response = await httpClient.SendRequestAsync(request);
+            string responseString = await response.Content.ReadAsStringAsync();
+        }
+
+        private async Task UploadImages(HttpStreamContent streamContent)
+        {
+            string posturi = "https://upload.api.weibo.com/2/statuses/upload_pic.json?access_token=" + App.weico_access_token;
+            HttpClient httpClient = new HttpClient();
+            HttpMultipartFormDataContent fileContent = new HttpMultipartFormDataContent();
+            fileContent.Add(streamContent, "pic", "pic.jpg");
+            HttpResponseMessage response = await httpClient.PostAsync(new Uri(posturi), fileContent);
+            string a = await response.Content.ReadAsStringAsync();
+            JObject o = JObject.Parse(a);
+            pic_ids += (string)o["pic_id"];
+            pic_ids += ",";
         }
 
         private async void ShowStatusBar()
