370aacb3cfbd54628d17a585b46090b99208deb9
Junda Yin <yinjunda.cool@gmail.com>
2016-05-11 02:42:26 -0400

    Add PullToRefresh

--- WooBee-Normal/XAML/TimeLine.xaml.cs
+++ WooBee-Normal/XAML/TimeLine.xaml.cs
@@ -5,6 +5,7 @@ using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -19,6 +20,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.Web.Http;
+using System.Threading.Tasks;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -34,6 +36,21 @@ namespace WooBee_Normal
         IncrementalSource incrementalSource = new IncrementalSource(sinceid);
         private static int sinceid = 0;
 
+        CompositionPropertySet _scrollerViewerManipulation;
+        ScrollViewer _scrollViewer;
+        Compositor _compositor;
+        ExpressionAnimation _rotationAnimation, _opacityAnimation, _offsetAnimation;
+        ScalarKeyFrameAnimation _resetAnimation, _loadingAnimation;
+
+        Visual _borderVisual;
+        Visual _refreshIconVisual;
+        float _refreshIconOffsetY;
+        const float REFRESH_ICON_MAX_OFFSET_Y = 36.0f;
+
+        bool _refresh;
+        DateTime _pulledDownTime, _restoredTime;
+        LinearEasingFunction _linear;
+
 
         public TimeLine()
         {
@@ -41,6 +58,154 @@ namespace WooBee_Normal
             listView.ItemsSource = incrementalSource;
             ShowStatusBar();
             GetNewMessage();
+
+            this.Loaded += (s, e) =>
+            {
+                _scrollViewer = listView.GetScrollViewer();
+                _scrollViewer.DirectManipulationStarted += OnDirectManipulationStarted;
+                _scrollViewer.DirectManipulationCompleted += OnDirectManipulationCompleted;
+
+                _scrollerViewerManipulation = ElementCompositionPreview.GetScrollViewerManipulationPropertySet(_scrollViewer);
+                _compositor = _scrollerViewerManipulation.Compositor;
+
+                _rotationAnimation = _compositor.CreateExpressionAnimation("min(max(0, ScrollManipulation.Translation.Y) * Multiplier, MaxDegree)");
+                _rotationAnimation.SetScalarParameter("Multiplier", 10.0f);
+                _rotationAnimation.SetScalarParameter("MaxDegree", 360.0f);
+                _rotationAnimation.SetReferenceParameter("ScrollManipulation", _scrollerViewerManipulation);
+
+                _opacityAnimation = _compositor.CreateExpressionAnimation("min(max(0, ScrollManipulation.Translation.Y) / Divider, 1)");
+                _opacityAnimation.SetScalarParameter("Divider", 30.0f);
+                _opacityAnimation.SetReferenceParameter("ScrollManipulation", _scrollerViewerManipulation);
+
+                _offsetAnimation = _compositor.CreateExpressionAnimation("(min(max(0, ScrollManipulation.Translation.Y) / Divider, 1)) * MaxOffsetY");
+                _offsetAnimation.SetScalarParameter("Divider", 30.0f);
+                _offsetAnimation.SetScalarParameter("MaxOffsetY", REFRESH_ICON_MAX_OFFSET_Y);
+                _offsetAnimation.SetReferenceParameter("ScrollManipulation", _scrollerViewerManipulation);
+
+                _resetAnimation = _compositor.CreateScalarKeyFrameAnimation();
+                _resetAnimation.InsertKeyFrame(1.0f, 0.0f);
+
+
+                _loadingAnimation = _compositor.CreateScalarKeyFrameAnimation();
+                _loadingAnimation.InsertKeyFrame(0.00f, 0.00f, _linear);
+                _loadingAnimation.InsertKeyFrame(0.20f, 72.0f, _linear);
+                _loadingAnimation.InsertKeyFrame(0.40f, 144.00f, _linear);
+                _loadingAnimation.InsertKeyFrame(0.60f, 216.0f, _linear);
+                _loadingAnimation.InsertKeyFrame(0.80f, 288.00f, _linear);
+                _loadingAnimation.InsertKeyFrame(1.00f, 360.0f, _linear);
+                _loadingAnimation.Duration = TimeSpan.FromMilliseconds(250);
+                _loadingAnimation.IterationBehavior = AnimationIterationBehavior.Forever;
+
+
+                _refreshIconVisual = ElementCompositionPreview.GetElementVisual(RefreshIcon);
+
+                _refreshIconVisual.CenterPoint = new Vector3(Convert.ToSingle(RefreshIcon.ActualWidth / 2), Convert.ToSingle(RefreshIcon.ActualHeight / 2), 0);
+
+
+                var border = (Border)VisualTreeHelper.GetChild(listView, 0);
+                _borderVisual = ElementCompositionPreview.GetElementVisual(border);
+
+                PrepareExpressionAnimationsOnScroll();
+            };
+
+            this.Unloaded += (s, e) =>
+            {
+                _scrollViewer.DirectManipulationStarted -= OnDirectManipulationStarted;
+                _scrollViewer.DirectManipulationCompleted -= OnDirectManipulationCompleted;
+            };
+        }
+
+        private void PrepareExpressionAnimationsOnScroll()
+        {
+            _refreshIconVisual.StartAnimation("RotationAngleinDegrees", _rotationAnimation);
+            _refreshIconVisual.StartAnimation("Opacity", _opacityAnimation);
+            _refreshIconVisual.StartAnimation("Offset.Y", _offsetAnimation);
+            _borderVisual.StartAnimation("Offset.Y", _offsetAnimation);
+        }
+
+        private async void OnDirectManipulationCompleted(object sender, object e)
+        {
+            Windows.UI.Xaml.Media.CompositionTarget.Rendering -= OnCompositionTargetRendering;
+
+            var cancelled = (_restoredTime - _pulledDownTime) > TimeSpan.FromMilliseconds(250);
+
+            if (_refresh)
+            {
+                if (cancelled)
+                {
+                    StartResetAnimations();
+                }
+                else
+                {
+                    await StartLoadingAnimation(() => StartResetAnimations());
+                }
+            }
+        }
+
+        void StartResetAnimations()
+        {
+            var batch = _compositor.CreateScopedBatch(CompositionBatchTypes.Animation);
+            
+            batch.Completed += (s, e) => PrepareExpressionAnimationsOnScroll();
+
+            _borderVisual.StartAnimation("Offset.Y", _resetAnimation);
+            _refreshIconVisual.StartAnimation("Opacity", _resetAnimation);
+            batch.End();
+        }
+
+        async Task StartLoadingAnimation(Action completed)
+        {
+            await Task.Delay(100);
+
+            _refreshIconVisual.StartAnimation("RotationAngleInDegrees", _loadingAnimation);
+
+            RefreshSource();
+
+            completed();
+        }
+
+        private void RefreshSource()
+        {
+            sinceid++;
+            IncrementalSource NewIncrementalSource = new IncrementalSource(sinceid);
+            listView.ItemsSource = NewIncrementalSource;
+            GetNewMessage();
+        }
+
+        private void OnCompositionTargetRendering(object sender, object e)
+        {
+            _refreshIconVisual.StopAnimation("Offset.Y");
+
+            _refreshIconOffsetY = _refreshIconVisual.Offset.Y;
+
+            if (!_refresh)
+            {
+                _refresh = _refreshIconOffsetY == REFRESH_ICON_MAX_OFFSET_Y;
+            }
+
+            if (_refreshIconOffsetY == REFRESH_ICON_MAX_OFFSET_Y)
+            {
+                _pulledDownTime = DateTime.Now;
+                
+                _refreshIconVisual.StopAnimation("Opacity");
+                _borderVisual.StopAnimation("Offset.Y");
+            }
+
+            if (_refresh && _refreshIconOffsetY <= 1)
+            {
+                _restoredTime = DateTime.Now;
+                
+            }
+
+            _refreshIconVisual.StartAnimation("Offset.Y", _offsetAnimation);
+        }
+
+        private void OnDirectManipulationStarted(object sender, object e)
+        {
+            Windows.UI.Xaml.Media.CompositionTarget.Rendering += OnCompositionTargetRendering;
+
+            
+            _refresh = false;
         }
 
         private void AppBarButton_Click(object sender, RoutedEventArgs e)
