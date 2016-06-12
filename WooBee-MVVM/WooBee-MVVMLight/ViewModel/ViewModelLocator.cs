cf967d34c79467733ed427d324412b77ba3368c9
yinju <yinju@DESKTOP-S7F7QVR>
2016-06-11 23:52:40 -0400

    Delete Unrelated Code

--- WooBee-MVVM/WooBee-MVVMLight/ViewModel/ViewModelLocator.cs
+++ WooBee-MVVM/WooBee-MVVMLight/ViewModel/ViewModelLocator.cs
@@ -13,28 +13,7 @@ namespace WooBee_MVVMLight.ViewModel
         static ViewModelLocator()
         {
             ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
-
-            var nav = new NavigationService();
-            nav.Configure(SecondPageKey, typeof(SecondPage));
-            SimpleIoc.Default.Register<INavigationService>(() => nav);
-
-            SimpleIoc.Default.Register<IDialogService, DialogService>();
-
-            if (ViewModelBase.IsInDesignModeStatic)
-            {
-                SimpleIoc.Default.Register<IDataService, Design.DesignDataService>();
-            }
-            else
-            {
-                SimpleIoc.Default.Register<IDataService, DataService>();
-            }
-
-            SimpleIoc.Default.Register<MainViewModel>();
         }
-
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance",
-            "CA1822:MarkMembersAsStatic",
-            Justification = "This non-static member is needed for data binding purposes.")]
-        public MainViewModel Main => ServiceLocator.Current.GetInstance<MainViewModel>();
+        
     }
 }
