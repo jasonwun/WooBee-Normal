b9cf799d91a8cab0cdb904e8540ff9142e9f430a
yinju <yinju@DESKTOP-BQAG589>
2016-08-03 15:12:06 +0800

    New Control

---
+++ WooBee-MVVM/WooBee-MVVMLight/Common/Control/FloatingActionButton.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Composition;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Hosting;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace WooBee_MVVMLight
+{
+    public sealed partial class FloatingActionButton : UserControl
+    {
+
+        public FloatingActionButton()
+        {
+            this.InitializeComponent();
+            _compositor = ElementCompositionPreview.GetElementVisual(this).Compositor;
+            _popUp = ElementCompositionPreview.GetElementVisual(PopUp);
+        }
+
+        private async void FloatingButton_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (!IsOpened)
+            {
+                PopUp.Visibility = Visibility.Visible;
+                ScalarKeyFrameAnimation opacityAnim = PopUpOpacityShowUpAnimation();
+                ScalarKeyFrameAnimation offsetAnim = PopUpOffsetShowUpAnimation();
+
+                _popUp.StartAnimation("Opacity", opacityAnim);
+                _popUp.StartAnimation("Offset.Y", offsetAnim);
+
+
+                IsOpened = true;
+            }
+
+            else
+            {
+                ScalarKeyFrameAnimation offsetAnim = PopUpOffsetDisableAnimation();
+                ScalarKeyFrameAnimation opacityAnim = PopUpOpacityDisableAnimation();
+
+
+                _popUp.StartAnimation("Opacity", opacityAnim);
+                _popUp.StartAnimation("Offset.Y", offsetAnim);
+
+                await Task.Delay(TimeSpan.FromMilliseconds(300));
+
+                IsOpened = false;
+
+                PopUp.Visibility = Visibility.Collapsed;
+            }
+
+        }
+
+        #region PopUpAnimation
+        private ScalarKeyFrameAnimation PopUpOffsetShowUpAnimation()
+        {
+            var offsetAnim = _compositor.CreateScalarKeyFrameAnimation();
+            offsetAnim.InsertKeyFrame(0.0f, 5.0f);
+            offsetAnim.InsertKeyFrame(1.0f, 0.0f);
+            offsetAnim.Duration = TimeSpan.FromMilliseconds(200);
+            return offsetAnim;
+        }
+        private ScalarKeyFrameAnimation PopUpOpacityShowUpAnimation()
+        {
+            var opacityAnim = _compositor.CreateScalarKeyFrameAnimation();
+            opacityAnim.InsertKeyFrame(0.0f, 0.0f);
+            opacityAnim.InsertKeyFrame(1.0f, 1.0f);
+            opacityAnim.Duration = TimeSpan.FromMilliseconds(200);
+            return opacityAnim;
+        }
+        private ScalarKeyFrameAnimation PopUpOffsetDisableAnimation()
+        {
+            var offsetAnim = _compositor.CreateScalarKeyFrameAnimation();
+            offsetAnim.InsertKeyFrame(0.0f, 0.0f);
+            offsetAnim.InsertKeyFrame(1.0f, 10.0f);
+            offsetAnim.Duration = TimeSpan.FromMilliseconds(200);
+            return offsetAnim;
+        }
+        private ScalarKeyFrameAnimation PopUpOpacityDisableAnimation()
+        {
+            var opacityAnim = _compositor.CreateScalarKeyFrameAnimation();
+            opacityAnim.InsertKeyFrame(0.0f, 1.0f);
+            opacityAnim.InsertKeyFrame(1.0f, 0.0f);
+            opacityAnim.Duration = TimeSpan.FromMilliseconds(200);
+            return opacityAnim;
+        } 
+        #endregion
+
+
+        bool IsOpened = false;
+        Compositor _compositor;
+        Visual _popUp;
+    }
+}
